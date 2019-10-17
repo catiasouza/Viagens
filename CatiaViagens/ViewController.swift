e0c48fa2a369bd86f84b96b45ad89621025357c3
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-17 16:11:09 -0300

    Criando alista de viagens

--- CatiaViagens/ViewController.swift
+++ CatiaViagens/ViewController.swift
@@ -1,10 +1,4 @@
-//
-//  ViewController.swift
-//  CatiaViagens
-//
-//  Created by Catia Miranda de Souza on 17/10/19.
-//  Copyright © 2019 Catia Miranda de Souza. All rights reserved.
-//
+
 
 import UIKit
 
