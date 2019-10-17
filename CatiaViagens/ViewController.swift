f134682e5095f1fe1361708e403c5af759a508d1
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-17 16:39:44 -0300

    Permissoes

--- CatiaViagens/ViewController.swift
+++ CatiaViagens/ViewController.swift
@@ -1,14 +1,40 @@
 
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet weak var mapaLabel: MKMapView!
+    var gerenciadorLocalizacao = CLLocationManager()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configuraGerenciadorLocalizacao()
+        
     }
-
-
+    func configuraGerenciadorLocalizacao(){
+        
+        gerenciadorLocalizacao.delegate = self
+        gerenciadorLocalizacao.desiredAccuracy = kCLLocationAccuracyBest
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()
+        gerenciadorLocalizacao.startUpdatingLocation()
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedWhenInUse{
+            
+            let alertaController = UIAlertController(title: "Permissao de Localizacao",
+                                                     message: "Necessario a permissao para acesso a sua localizacao,por favor habilite.", preferredStyle: .alert)
+            
+            let acaoConfiguracoes = UIAlertAction(title: "Abrir configuracoes", style: .default, handler: {(alertaConfiguracoes) in
+                if let configuracoes = NSURL(string: UIApplication.openSettingsURLString){
+                    UIApplication.shared.open( configuracoes as URL)
+                }
+            })
+        }
+    }
 }
 
